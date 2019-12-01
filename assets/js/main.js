adac6753d73c638794f5cffa7f56e74332f6cd31
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-12-01 17:17:52 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.getElementById("toggler").addEventListener("click",function(){
+    document.getElementById("side_nav").classList.toggle("show");
+})
+
+
+document.getElementById("toggle_close").addEventListener("click",function(){
+    document.getElementById("side_nav").classList.toggle("show");
+})
